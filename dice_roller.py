4840f470ecf624875629d7b7472eab6d3cef6100
SJCY-33 <sjcy-33@CYS33-KALI01.cyber.int>
2021-02-18 09:47:36 -0400

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 def main():
     import random
-    dice_rolls = 2
+    dice_rolls = int(input('How many dice would you like to roll'))
+    dice_size = int(input('How many sides are the dice?'))
     dice_sum = 0
     for i in range(0,dice_rolls):
     
-        roll = random.randint(1,6)
+        roll = random.randint(1,dice_size)
         dice_sum += roll
-        print('You rolled a {0}'.format(roll))
+        if roll == 1: 
+            print('You rolled a {0}! Critical Fail!'.format(roll))
+        elif roll == dice_size:
+            print('Your rolled a {0}! Critical Success!'.format(roll))
+        else:
+            print('You rolled a {0}'.format(roll))
     print('You have rolled a total of {0}'.format(dice_sum))
 
 if __name__== "__main__":
